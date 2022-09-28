c35bf62722d119dfe26f0ecbe28847dcd2081d8e
Inacio Oliveira <inacio.carvalho.oliveira02@gmail.com>
2022-09-28 08:08:07 -0300

    prática com listas

---
+++ listas/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*
+            List - lista tudo 
+            IEnumerable 
+
+            
+            */
+            var pagamentos = new IEnumerable<Pagamento>();
+            pagamentos.Add(new Pagamento(0));
+            pagamentos.Add(new Pagamento(1));
+            pagamentos.Add(new Pagamento(2));
+            pagamentos.Add(new Pagamento(3));
+
+            foreach (var item in pagamentos)
+            {
+                Console.WriteLine(item.Id);
+            }
+            /*
+            listagem modos: 
+            Where: 
+            
+            
+            */                     
+            var pagamentoA = pagamentos.Where(x => x.Id == 3);  
+            Console.WriteLine(pagamentoA.Id);          
+        }
+    }
+    public class Pagamento
+    {
+        public int Id { get; set; }
+        public Pagamento(){}
+        public Pagamento(int id)
+        {
+            this.Id = id;
+        }
+        
+        
+    }
+}
