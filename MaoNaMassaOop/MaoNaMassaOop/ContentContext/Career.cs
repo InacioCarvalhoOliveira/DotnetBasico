fae6e3f4da43ae4e9ece8602c42c674860b6209f
Inacio Oliveira <inacio.carvalho.oliveira02@gmail.com>
2022-09-29 14:55:38 -0300

    ...enumeradores

---
+++ MaoNaMassaOop/MaoNaMassaOop/ContentContext/Career.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace MaoNaMassaOop.ContentContext
+{
+    public class Careers : Content
+    {
+        public Careers()
+        {
+            Items = new List<CareerItem>(); 
+        }
+        public IList<CareerItem> Items { get; set; }
+        public int TotalCourses => Items.Count;
+
+        var articles = new 
+        
+        
+    }
+    
+}
