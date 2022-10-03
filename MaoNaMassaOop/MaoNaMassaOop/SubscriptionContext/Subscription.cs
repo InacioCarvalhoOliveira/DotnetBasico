4d46156d82065ab71aa5b96bf7fbc9d40632bd77
Inacio Oliveira <inacio.carvalho.oliveira02@gmail.com>
2022-10-03 10:26:26 -0300

    resolvendo erros

--- MaoNaMassaOop/MaoNaMassaOop/SubscriptionContext/Subscription.cs
+++ MaoNaMassaOop/MaoNaMassaOop/SubscriptionContext/Subscription.cs
@@ -5,7 +5,7 @@ namespace MaoNaMassaOop.SubscriptionContext
 {
     public class Subscription : Base
     {     
-        public Plan Plan { get; set; } 
+       // public Plan Plan { get; set; } 
         public DateTime? EndDate { get; set; }
         public bool IsInactive => EndDate <= DateTime.Now;
         
